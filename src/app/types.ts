116e23c4afc35f5347b45588ed68a1e9325243ee
Zoltan Dalmadi <dmz985@gmail.com>
2020-11-09 12:31:53 +0100

    Add some interfaces

--- src/app/types.ts
+++ src/app/types.ts
@@ -1,10 +1,34 @@
-interface MainState {}
+export const enum Role {
+  Administrator = 0,
+  Guest = 1,
+  SilverUser = 2,
+  GoldUser = 3,
+}
 
-interface ProfileState {}
+export interface User {
+  id: number;
+  name: string;
+  password: string;
+  email: string;
+  role: Role;
+}
 
-interface AdminState {}
+export interface UserResponse {
+  status: number;
+  message: string;
+  data: User[];
+}
 
-interface AppState {
+export interface ProfileState {
+  users: User[];
+  currentUser: User;
+}
+
+export interface MainState {}
+
+export interface AdminState {}
+
+export interface AppState {
   main: MainState;
   profile: ProfileState;
   admin: AdminState;
