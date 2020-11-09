82e3410b291846619fc6656cc909ae01f1f0ae31
Zoltan Dalmadi <dmz985@gmail.com>
2020-11-09 10:46:23 +0100

    Added ngrx packages & page skeleton components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminPageComponent, ProfilePageComponent } from 'src/app/pages';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProfilePageComponent },
+  { path: 'admin', component: AdminPageComponent },
+  { path: 'profile', component: ProfilePageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/pages/index.ts
@@ -0,0 +1,3 @@
+export * from './admin-page/admin-page.component';
+export * from './profile-page/profile-page.component';
+export * from './main-page/main-page.component';
